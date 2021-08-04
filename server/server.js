bcdd3ecc31fb14e2b2df968eac79b12c0a10169d
mcc <costanza+git@gmail.com>
2021-08-04 16:47:53 -0700

    add mutations for users, thoughts and reactions; add JWT authentication

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
-// import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
@@ -12,7 +11,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate Apollo server with the Express applicaiton as middleware
