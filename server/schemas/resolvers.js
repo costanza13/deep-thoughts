b34ec070ca9747f15dce6c6f35d8c3d7d7b7e9dd
mcc <costanza+git@gmail.com>
2021-08-05 20:38:01 -0700

    add personalization features (friend list, "me" page)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
-const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -16,29 +16,25 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
-    // get all thoughts, optionally filtered by username
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    // get a thought by id
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
-    },
-    // get all users
     users: async () => {
       return User.find()
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
-    // get a user by username
     user: async (parent, { username }) => {
       return User.findOne({ username })
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    }
   },
 
   Mutation: {
@@ -56,6 +52,7 @@ const resolvers = {
       }
 
       const correctPw = await user.isCorrectPassword(password);
+
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
